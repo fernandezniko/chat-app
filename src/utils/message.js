c418ddd75f191ff7c9eb8185581112b53afe1a5a
fernandezniko <ni.ko94@hotmail.com>
2021-01-17 02:56:06 -0300

    Primera version de la app

---
+++ src/utils/message.js
@@ -0,0 +1,22 @@
+const generateMessage = (username, text) => {
+
+    return {
+        username,
+        text: text,
+        createdAt: new Date().getTime()
+    }
+}
+
+const generateLocationMessage = (username, url) => {
+
+    return {
+        username,
+        url: url,
+        createdAt: new Date().getTime()
+    }
+}
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
